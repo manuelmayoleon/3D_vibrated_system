ac205f3e5a36898c77607b1c278bbedc191338ed
manuelmayoleon <manuelmayoleon09@gmail.com>
2022-11-11 14:00:10 +0100

    v1 stationary solved

--- trans_eq.py
+++ trans_eq.py
@@ -33,10 +33,16 @@ ba_sim29 = pd.read_csv("dataBeta/beta_H29.dat",header=0,sep='\s+')
 
 betas = np.linspace(0.01,10.0,1000)
 alpha = np.linspace(0.010,1.000,1000)
-class Panel:
-    def __init__(self,a=0.950):
-        self.a = a
 
+sigma = 1.0
+rho = 0.02*sigma
+
+class Panel:
+    def __init__(self,a=0.950,sigma=1.0):
+        self.a = a
+        self.sigma = sigma
+    def htilde(self,dens,h):
+            return h*dens*self.sigma**2
     def polinomio(self,b):
         return np.sqrt(b+1)*(4 * ( 1 + self.a ) * b ** 2 + ( 16 * self.a - 8 ) * b + ( 9 - 3 * self.a ) )/( ( 30 - 18 * self.a) * b + ( 9 -3 * self.a ) )
     def  bets_1(alpha):   
@@ -47,34 +53,69 @@ class Panel:
             return 32*(1-alpha)/(5+9*alpha)
     def transcendental_eq(self,b):
         return  np.sqrt(b+1)*(4 * ( 1 + self.a ) * b ** 2 + ( 16 * self.a - 8 ) * b + ( 9 - 3 * self.a ) )/( ( 30 - 18 * self.a) * b + ( 9 -3 * self.a ) )- np.arcsinh(np.sqrt(b))/np.sqrt(b)
-# def f(x):
-#     c = polinomio(x[0],x[1])- np.arcsinh(np.sqrt(x[1]))/np.sqrt(x[1])
-#     d = x[0]
-#     return [c, d]
+    def b1(alfa,b):
+        return  - np.arcsinh( np.sqrt(b) ) * ( 12 * b + ( 9 - 3 * alfa ) ) / np.sqrt(b)  
+    def b2(alfa,b):
+            return np.sqrt(1+b )*(( 16 - 8 * alfa ) * b**2 + ( 22 - 14 * alfa ) * b + ( 9 - 3 * alfa ) )
+    def btilde(alfa,b): 
+          return (1+alfa)*(Panel.b1(alfa,b) + Panel.b2(alfa,b))/( 96 * b ) 
+    def btilde(alfa,b): 
+          return (1+alfa)*(Panel.b1(alfa,b) + Panel.b2(alfa,b))/( 96 * b )
+    def b_coef(self,alfa,b,dens,h):
+        return (1+b)/(np.sqrt(2*np.pi)*Panel.btilde(alfa,b)*Panel().htilde(dens,h))
+    def v1s_w(self,alfa,b,dens,h):
+        return 0.5 * Panel().b_coef(alfa,b,dens,h)*( 1 + np.sqrt(1 + 8 / ( Panel().b_coef(alfa,b,dens,h)*np.sqrt((1+b)*np.pi) ) ) )
+    
+# ! SOLVE TRANSCENDENTAL EQUATION USING FSOLVE USING Powell's dog leg method
+# ?? Powell's dog leg method is an iterative optimisation algorithm for the solution of non-linear least squares problems. 
+# ??       it combines the Gauss–Newton algorithm with gradient descent, but it uses an explicit trust region. 
 
 sol = np.zeros(len(alpha))    
+v1sw_h10 = np.zeros(len(alpha))
+v1sw_h12 = np.zeros(len(alpha))
+v1sw_h14 = np.zeros(len(alpha))
+v1sw_h29 = np.zeros(len(alpha))
 i=0
 for x in alpha:
     
 
     sol[i] = fsolve(Panel(x).transcendental_eq ,[4.0] )
+    
+    v1sw_h10[i] = Panel().v1s_w(x,sol[i],rho,10)
+    v1sw_h12[i] = Panel().v1s_w(x,sol[i],rho,12)
+    v1sw_h14[i] = Panel().v1s_w(x,sol[i],rho,14)
+    v1sw_h29[i] = Panel().v1s_w(x,sol[i],rho,29)
+   
+    # print(alpha[i]) 
     i+=1
+        
     # print(result.x)
 # print(sol)
 
+
+
+
+fig22 = plt.plot(figsize=(14,8))
+plt.plot(alpha[0:len(alpha)-1],v1sw_h10[0:len(alpha)-1],color= "C0")
+plt.plot(alpha[0:len(alpha)-1],v1sw_h12[0:len(alpha)-1],color= "C1")
+plt.plot(alpha[0:len(alpha)-1],v1sw_h14[0:len(alpha)-1],color= "C2")
+plt.plot(alpha[0:len(alpha)-1],v1sw_h29[0:len(alpha)-1],color= "C3")
+# plt.plot(betas,Panel.b1(0.99,betas),color= "C1")
+# plt.plot(betas,Panel.b2(0.99,betas),color= "C2")
+plt.xlim(0.75,1.0)
+plt.ylim(0,1000)
+# print(Panel.b1(0.99 ,betas))
+
+# ! SOLVE TRANSCENDENTAL EQUATION USING graphical method 
 # plt.plot(betas,np.arcsinh(np.sqrt(betas))/np.sqrt(betas),color= "C0")
-
-
+ 
 # for x in alpha:
-    
+
 #     r = random.random()
-
 #     b =      random.random()
-
 #     g = random.random()
 
 #     color = (r, g, b)
-    
     
 #     plt.plot(betas,polinomio(x,betas) ,linestyle = ":",linewidth=1.5,color=color,label= r"$\alpha = $ %2.2f" % x)
     # for y in betas :
@@ -86,8 +127,6 @@ for x in alpha:
     #         with open('b_a.txt', 'a',newline='\n') as f:
     #                 writer = csv.writer(f, delimiter='\t')
     #                 writer.writerows(zip([x],[y]))
-
-
 
 # plt.grid(color='k', linestyle='--', linewidth=0.5,alpha=0.2)
 
@@ -105,10 +144,10 @@ for x in alpha:
 
 fig, ax1=plt.subplots(figsize=(14,8))
 
-# ax1.plot(alpha,Panel.bets_1(alpha) ,linewidth=1.5,color="C1" )
+ax1.plot(alpha,Panel.bets_1(alpha) ,linewidth=1.5,alpha=0.6,color="C1" )
 
-# ax1.plot(alpha,Panel.bets_2(alpha) ,linewidth=1.5,color="C2")
-ax1.plot(alpha,sol ,linewidth=1,linestyle= "",color="C10")
+ax1.plot(alpha,Panel.bets_2(alpha) ,linewidth=1.5,alpha =0.6,color="C2")
+ax1.plot(alpha,sol ,linewidth=1.5,linestyle= "dashdot",color='xkcd:crimson')
 
 # plt.plot(betas,np.arcsinh(np.sqrt(betas))/np.sqrt(betas),color= "C0")
 
@@ -139,21 +178,23 @@ ax1.set_title( r' \textbf {Relación $\beta \leftrightarrow \alpha$}' ,fontsize=
 # Create a set of inset Axes: these should fill the bounding box allocated to
 # them.
 ax2 = plt.axes([0,0,1,1])
+
+
 # Manually set the position and relative size of the inset axes within ax1
+
+
 ip = InsetPosition(ax1, [0.45,0.45,0.5,0.5])
+
+
 ax2.set_axes_locator(ip)
-# Mark the region corresponding to the inset axes on ax1 and draw lines
-# in grey linking the two axes.
-# mark_inset(ax1, ax2, loc1=2, loc2=4, fc="none", ec='0.5')
 
-# The data: only display for low temperature in the inset figure.
-ax2.plot(alpha,Panel.bets_1(alpha) ,linewidth=1.5,color="C1", label= r"$\beta_s$  $\mathcal{O}(\beta^2)$ integral aprox." )
+ax2.plot(alpha,Panel.bets_1(alpha) ,linewidth=1.5,color="C1",alpha=0.6, label= r"$\beta_s$  $\mathcal{O}(\beta^2)$ integral aprox." )
 
-ax2.plot(alpha,Panel.bets_2(alpha) ,linewidth=1.5,color="C2",label= r"$\beta_s$  $\mathcal{O}(\beta^2)$ integral exacta" )
+ax2.plot(alpha,Panel.bets_2(alpha) ,linewidth=1.5,color="C2",alpha =0.6,label= r"$\beta_s$  $\mathcal{O}(\beta^2)$ integral exacta" )
 
 ax2.plot(ba['alpha'],ba['beta'],linewidth=1.5,linestyle = ":",color= "C0",label = r"$\beta_s$  exacta (sol. gráfica)")
 
-ax2.plot(alpha,sol ,linewidth=1.5,linestyle= "",color="C10",label = r"$\beta_s$  integral exacta" )
+ax2.plot(alpha,sol ,linewidth=1,linestyle= "dashdot",color='xkcd:crimson',label = r"$\beta_s$  integral exacta" )
 
 ax2.errorbar(ba_sim8['alpha'], ba_sim8['beta'], yerr=ba_sim8['errorbeta'], color='C3',marker="o",linestyle="",label=r"$\beta_s$, $h= 8\sigma$ (MD)") 
 ax2.errorbar(ba_sim10['alpha'], ba_sim10['beta'], yerr=ba_sim10['errorbeta'], color='C4',marker="o",linestyle="",label=r"$\beta_s$, $h= 10\sigma$ (MD)") 
@@ -166,8 +207,8 @@ ax2.legend(loc=0)
 
 # Some ad hoc tweaks.
 
-ax2.set_xlim(0.6,1.0)
-ax2.set_ylim(0.0,2.0)
+ax2.set_xlim(0.77,1.0)
+ax2.set_ylim(0.0,0.77)
 
 
 
